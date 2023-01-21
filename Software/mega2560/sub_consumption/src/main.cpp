1d4ef99a283134a2f786ca547f8edd0af066d715
Nathan Gardner <nathan.gardner025@gmail.com>
2023-01-21 16:29:40 -0600

    ranamed topics for clarity

--- Software/mega2560/sub_consumption/src/main.cpp
+++ Software/mega2560/sub_consumption/src/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
  * @author Nathan Gardner
- * @brief ROS node P/S to stateConsumption for motor remote control
+ * @brief ROS node P/S to /consumption/motorState for motor remote control
  * @version 0.1
  * @date 2023-01-21
  * 
@@ -23,14 +23,14 @@
 void consumptionCallback(const std_msgs::UInt8& msg);
 
 ros::NodeHandle nh;
-std_msgs::UInt8 u8_stateConsumption;
+std_msgs::UInt8 u8_stateMotorConsumption;
 
-ros::Publisher consumption_pub("stateConsumption", &u8_stateConsumption);
-ros::Subscriber<std_msgs::UInt8> consumption_sub("consumption_sub", &consumptionCallback);
+ros::Publisher consumption_pub("/consumption/motorState", &u8_stateMotorConsumption);
+ros::Subscriber<std_msgs::UInt8> consumption_sub("/consumption/cmdMotorState", &consumptionCallback);
 
 void consumptionCallback(const std_msgs::UInt8& msg){
   // update motor state and analog pin
-  u8_stateConsumption.data = msg.data;
+  u8_stateMotorConsumption.data = msg.data;
   analogWrite(PWM1, msg.data);
 }
 
@@ -43,13 +43,13 @@ void setup()
   nh.subscribe(consumption_sub);
 
   // initialize motor state to false and motor state to off
-  u8_stateConsumption.data = 0;
+  u8_stateMotorConsumption.data = 0;
   consumptionMotorOff;
 }
 
 void loop()
 {
-  consumption_pub.publish( &u8_stateConsumption );
+  consumption_pub.publish( &u8_stateMotorConsumption );
   nh.spinOnce();
   delay(1000);
 }
