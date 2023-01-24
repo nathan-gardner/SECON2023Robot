f735e7e3dd4d5ef5a0c873a6a1d0a5270bb863e9
Nathan Gardner <30758520+nathan-gardner@users.noreply.github.com>
2023-01-23 18:50:00 -0600

    Consumption Control Mega2560 (#31)
    * added consumption control ros node
    
    * changed msg type from bool to uint8 for motor cmd
    
    * removed unneeded code
    
    * ranamed topics for clarity
    
    * updated comment

---
+++ Software/mega2560/sub_consumption/src/main.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file main.cpp
+ * @author Nathan Gardner
+ * @brief ROS node P/S to /consumption/motorState and consumption/cmdMotorState for motor remote control
+ * @version 0.1
+ * @date 2023-01-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef MAIN_CPP
+#define MAIN_CPP
+
+#include <Arduino.h>
+#include <ros.h>
+#include <std_msgs/UInt8.h>
+
+#define PWM1 2
+#define consumptionMotorOff analogWrite(PWM1, 0)
+
+// Prototypes
+void consumptionCallback(const std_msgs::UInt8& msg);
+
+ros::NodeHandle nh;
+std_msgs::UInt8 u8_stateMotorConsumption;
+
+ros::Publisher consumption_pub("/consumption/motorState", &u8_stateMotorConsumption);
+ros::Subscriber<std_msgs::UInt8> consumption_sub("/consumption/cmdMotorState", &consumptionCallback);
+
+void consumptionCallback(const std_msgs::UInt8& msg){
+  // update motor state and analog pin
+  u8_stateMotorConsumption.data = msg.data;
+  analogWrite(PWM1, msg.data);
+}
+
+void setup()
+{
+  pinMode(PWM1, OUTPUT);
+
+  nh.initNode();
+  nh.advertise(consumption_pub);
+  nh.subscribe(consumption_sub);
+
+  // initialize motor state to false and motor state to off
+  u8_stateMotorConsumption.data = 0;
+  consumptionMotorOff;
+}
+
+void loop()
+{
+  consumption_pub.publish( &u8_stateMotorConsumption );
+  nh.spinOnce();
+  delay(1000);
+}
+
+#endif
