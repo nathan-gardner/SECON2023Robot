d5acf1671fe1edf5b691fca0189926586de751f8
nathan-gardner <nathan.gardner025@gmail.com>
2023-02-22 21:42:34 -0600

    git init add locomotion function

--- Software/mega2560/sub_consumption/src/main.cpp
+++ Software/mega2560/sub_consumption/src/main.cpp
@@ -15,18 +15,37 @@
 #include <Arduino.h>
 #include <ros.h>
 #include <std_msgs/UInt8.h>
+#include <std_msgs/UInt8MultiArray.h>
 
+// Consumption Defs
 #define PWM1 2
 #define consumptionMotorOff analogWrite(PWM1, 0)
 
+// Locomotion Defs
+#define PWMX 0 // 1 PWM pin ADD LATER
+#define LOCOMOTIONDIGITAL1 0 // 3 digital pins ADD LATER
+#define LOCOMOTIONDIGITAL2 0
+#define LOCOMOTIONDIGITAL3 0
+
 // Prototypes
 void consumptionCallback(const std_msgs::UInt8& msg);
+void locomotionCallback(const std_msgs::UInt8MultiArray& msg);
 
 ros::NodeHandle nh;
+
+// Consumption Data
 std_msgs::UInt8 u8_stateMotorConsumption;
 
+// Consumption Pub/Sub
 ros::Publisher consumption_pub("/consumption/motorState", &u8_stateMotorConsumption);
 ros::Subscriber<std_msgs::UInt8> consumption_sub("/consumption/cmdMotorState", &consumptionCallback);
+
+// Locomotion Data
+std_msgs::UInt8MultiArray au8_stateMotorLocomotion;
+
+// Locomotion Pub/Sub
+ros::Publisher locomotion_pub("/locomotion/motorState", &au8_stateMotorLocomotion);
+ros::Subscriber<std_msgs::UInt8MultiArray> locomotion_sub("/locomotion/cmdMotorState", &locomotionCallback);
 
 void consumptionCallback(const std_msgs::UInt8& msg){
   // update motor state and analog pin
@@ -34,22 +53,35 @@ void consumptionCallback(const std_msgs::UInt8& msg){
   analogWrite(PWM1, msg.data);
 }
 
+void locomotionCallback(const std_msgs::UInt8MultiArray& msg){
+  // insert callback here
+
+}
+
 void setup()
 {
   pinMode(PWM1, OUTPUT);
 
   nh.initNode();
+  // consumption
   nh.advertise(consumption_pub);
   nh.subscribe(consumption_sub);
+  // locomotion
+  nh.advertise(locomotion_pub);
+  nh.subscribe(locomotion_sub);
 
-  // initialize motor state to false and motor state to off
+  // initialize consumption motor state to false and motor state to off
   u8_stateMotorConsumption.data = 0;
   consumptionMotorOff;
+
+  //au8_stateMotorLocomotion.data = 0;
+  //analogWrite(PWMX, 0);
 }
 
 void loop()
 {
   consumption_pub.publish( &u8_stateMotorConsumption );
+  locomotion_pub.publish ( &au8_stateMotorLocomotion );
   nh.spinOnce();
   delay(1000);
 }
