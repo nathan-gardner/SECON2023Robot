d852a3902745c7ea82366212b1d1b879c8c5637b
mmkelly42 <112424739+mmkelly42@users.noreply.github.com>
2023-03-20 15:56:04 -0500

    added servo

--- Software/mega2560/mega2/src/main.cpp
+++ Software/mega2560/mega2/src/main.cpp
@@ -18,21 +18,29 @@
 
 #define solenoidPin 7
 #define solenoidPin2 8
+#define maestroSerial Serial1
+
+MicroMaestro maestro(maestroSerial);
 
 void setup() {
   pinMode(solenoidPin, OUTPUT);
   pinMode(solenoidPin2, OUTPUT);
-
+  maestroSerial.begin(9600);
+  maestro.setTarget(0, 6000);
 }
 
 void loop() {
   digitalWrite(solenoidPin, HIGH);
   digitalWrite(solenoidPin2, HIGH);
-  delay(2000);
+  maestro.setTargetMiniSSC(0, 254);
+  delay(500);
 
   digitalWrite(solenoidPin, LOW);
   digitalWrite(solenoidPin2, LOW);
+  maestro.setTargetMiniSSC(0, 0);
   delay(7000);
 }
+
+
 
 #endif
