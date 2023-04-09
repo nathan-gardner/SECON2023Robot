2cec31f70c9a25be1a9db944b548191654ad548b
Nathan Gardner <nathan.gardner025@gmail.com>
2023-04-09 15:38:38 -0500

    update top-level.py

--- Software/top-level/beginner_tutorials/scripts/top_level.py
+++ Software/top-level/beginner_tutorials/scripts/top_level.py
@@ -43,11 +43,29 @@ def pub_direction(x, y, z):
     cmd_vel_msg.angular.z = z
     cmd_vel_pub.publish(cmd_vel_msg)
 
+def left_feeding():
+    cmd_left_servo_pub_msg.data = "OPEN"
+    cmd_left_servo_pub.publish(cmd_left_servo_pub_msg)
+
+def right_feeding():
+    cmd_right_servo_pub_msg.data = "OPEN"
+    cmd_right_servo_pub.publish(cmd_right_servo_pub_msg)
+
 def extend_trailer():
     cmd_solenoid_pos_msg.data = True
     cmd_solenoid_pos_pub.publish(cmd_solenoid_pos_msg)
     sleep(1)
     cmd_servo_pos_msg.data = "EXTEND"
+    cmd_servo_pos_pub.publish(cmd_servo_pos_msg)
+    sleep(1)
+    cmd_solenoid_pos_msg.data = False
+    cmd_solenoid_pos_pub.publish(cmd_solenoid_pos_msg)
+
+def retract_trailer():
+    cmd_solenoid_pos_msg.data = True
+    cmd_solenoid_pos_pub.publish(cmd_solenoid_pos_msg)
+    sleep(1)
+    cmd_servo_pos_msg.data = "RETRACT"
     cmd_servo_pos_pub.publish(cmd_servo_pos_msg)
     sleep(1)
     cmd_solenoid_pos_msg.data = False
@@ -59,13 +77,25 @@ def turn_right():
     global fr_encoder
     fr_encoder = rospy.wait_for_message('/locomotion/encoder', Twist, timeout=None).data[1]
     pos = fr_encoder
-    while(abs(pos - fr_encoder) < 780):
+    while(abs(pos - fr_encoder) < 880):
         pub_direction(0,0,50)
         sleep(0.001)
         fr_encoder = rospy.wait_for_message('/locomotion/encoder', Twist, timeout=None).data[1]
     pub_direction(0,0,0)
     sleep(0.5)
 
+def turn_left():
+    pub_direction(0,0,0)
+    sleep(0.5)
+    global fr_encoder
+    fr_encoder = rospy.wait_for_message('/locomotion/encoder', Twist, timeout=None).data[1]
+    pos = fr_encoder
+    while(abs(pos - fr_encoder) < 880):
+        pub_direction(0,0,-50)
+        sleep(0.001)
+        fr_encoder = rospy.wait_for_message('/locomotion/encoder', Twist, timeout=None).data[1]
+    pub_direction(0,0,0)
+    sleep(0.5)
 
 def forward(clicks, speed):
     global fr_encoder
@@ -141,17 +171,25 @@ fr_encoder = 0
 rl_encoder = 0
 rr_encoder = 0
 
+delay = 1
+
 global cmd_vel_msg
 global cmd_servo_pos_msg
 global cmd_solenoid_pos_msg
+global cmd_left_servo_pub_msg
+global cmd_right_servo_pub_msg
 cmd_vel_msg = Twist()
 cmd_servo_pos_msg = String()
 cmd_solenoid_pos_msg = Bool()
+cmd_left_servo_pub_msg = String()
+cmd_right_servo_pub_msg = String()
 
 # initislize publishers and subscribers 
 cmd_vel_pub = rospy.Publisher('/locomotion/cmd_vel', Twist, queue_size=10)
 cmd_servo_pos_pub = rospy.Publisher('/duckstorage/cmd_servo_pos', String, queue_size=10)
 cmd_solenoid_pos_pub = rospy.Publisher('/duckstorage/cmd_solenoid_pos', Bool, queue_size=10)
+cmd_left_servo_pub = rospy.Publisher('/feeding/cmd_left_servo_pos', String, queue_size=10)
+cmd_right_servo_pub = rospy.Publisher('/feeding/cmd_right_servo_pos', String, queue_size=10)
 sub = rospy.Subscriber('/locomotion/encoder', Int32MultiArray, callback)
 start_sub = rospy.Subscriber('/start', Bool, start_callback)
 
@@ -165,103 +203,92 @@ while(start):
     sleep(0.1)
 
 # Extend the trailer at the beginning
-extend_trailer()
-
-sleep(1)
-forward(3500, 125)
-sleep(1)
-right(350, 100)
-sleep(1)
-backward(500, 100)
-sleep(1)
+# extend_trailer()
+sleep(delay)
+forward(3200, 100)
+sleep(delay)
+right(850, 125)
+sleep(delay)
+backward(800, 100)
+sleep(delay)
 turn_right()
+sleep(delay)
+backward(600, 75)
+sleep(delay)
+left(1000, 60)
+sleep(delay)
+left_feeding()
+sleep(delay)
+forward(2000, 100)
+sleep(delay)
+right(300, 75)
+sleep(delay)
+turn_left()
+sleep(delay)
+turn_left()
+sleep(delay)
+backward(200, 50)
+sleep(delay)
+right(300, 75)
+sleep(delay)
+right_feeding()
+sleep(delay)
+forward(200, 75)
+sleep(delay)
+turn_left()
+sleep(delay)
+turn_left()
+sleep(delay)
+backward(1700, 125)
+sleep(delay)
+right(400, 75)
+sleep(delay)
 turn_right()
-sleep(1)
-backward(800, 75)
-sleep(1)
-right(300, 100)
-sleep(1)
-backward(400, 50)
-sleep(1)
-forward(8000, 150) #slightly higher because of wall friction 
-sleep(1)
-backward(8000, 150) #slightly higher because of wall friction
-sleep(1)
-left(600, 100)
-sleep(1)
-backward(400, 50)
-sleep(1)
+sleep(delay)
+right(900, 50)
+sleep(delay)
+left(200, 60)
+sleep(delay)
 
-# first forward path
-forward(7400, 100)
-sleep(1)
-backward(7300, 100)
-sleep(1)
-backward(800, 50)
-sleep(1)
-left(600, 100)
-sleep(1)
-backward(400, 50)
-sleep(1)
 
-# second forward path
-forward(7400, 100)
-sleep(1)
-backward(7300, 100)
-sleep(1)
-backward(800, 50)
-sleep(1)
-left(600, 100)
-sleep(1)
-backward(400, 50)
-sleep(1)
+forward(6100, 100)
+sleep(delay)
+backward(5800, 100)
+sleep(delay)
 
-# third forward path
-forward(7400, 100)
-sleep(1)
-backward(7300, 100)
-sleep(1)
-left(600,100)
-sleep(1)
-backward(400, 50)
-sleep(1)
+# second pass
+left(900, 125)
+sleep(delay)
+forward(6100, 100)
+sleep(delay)
+backward(6000, 100)
+sleep(delay)
 
-# fourth forward path
-forward(7400, 100)
-sleep(1)
-backward(7300, 100)
-sleep(1)
-left(600,100)
-sleep(1)
-backward(400, 50)
-sleep(1)
+# third pass
+left(900, 125)
+sleep(delay)
+forward(6100, 100)
+sleep(delay)
+backward(6000, 100)
+sleep(delay)
 
-# fifth forward path
-forward(7400, 100)
-sleep(1)
-backward(7300, 100)
-sleep(1)
-left(600,100)
-sleep(1)
-backward(400, 50)
-sleep(1)
+# fourth pass
+left(900, 125)
+sleep(delay)
+forward(6200, 100)
+sleep(delay)
 
-# sixth forward path
-forward(7400, 100)
-sleep(1)
-backward(7300, 100)
-sleep(1)
-left(600,100)
-sleep(1)
-backward(400, 50)
-sleep(1)
-
-# seventh forward path
-forward(7400, 100)
-sleep(1)
-backward(7300, 100)
-sleep(1)
-left(600,100)
-sleep(1)
-backward(400, 50)
-sleep(1)
+# duck drop
+backward(2700, 100)
+sleep(delay)
+left(500, 50)
+sleep(delay)
+right(400, 75)
+sleep(delay)
+# drop trailer
+forward(2600, 100)
+sleep(delay)
+forward(300, 50)
+sleep(delay)
+right(1200, 125)
+sleep(delay)
